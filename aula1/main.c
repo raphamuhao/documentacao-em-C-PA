b8ad355d224a5422e320526b5b81935611be7a82
Raphaela Munhão <79518019+raphamuhao@users.noreply.github.com>
2023-03-28 19:25:20 -0300

    Create main.c

---
+++ aula1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a,b,m;
+
+    printf("ola\n\n");
+    printf("digite dois numeros: ");
+    scanf("%f%f", &a, &b);
+    m =(a+b)/2;
+    printf("valor da media:  %f", m);
+    
+    return 0;
+}
